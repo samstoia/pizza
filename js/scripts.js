1385445bb188da6fdac5e9d5da68fcb4c666610e
samstoia <samstoia@gmail.com>
2019-02-01 12:51:35 -0800

    Create working prototype for calculating pizza price

--- js/scripts.js
+++ js/scripts.js
@@ -5,9 +5,27 @@ function Pizza(size, toppings) {
   this.toppings = toppings
 }
 
-Pizza.prototype.priceShow = function(pizzaPrice) {
-
+Pizza.prototype.priceCalculate = function(price, toppings) {
+  var price = 7;
+  if (this.size === 1) {
+    price -= 1;
+  }
+  if (this.size === 2) {
+    price += 1;
+  }
+  if (this.size === 3) {
+    price += 3;
+  }
+  else if (this.size !== 1 && this.size !== 2 && this.size !== 3) {
+    alert("YOU HAVE TO PICK A SIZE!")
+  }
+  for (var i=0; i < this.toppings.length; i++) {
+    price += 1;
+  }
+  this.price = price;
+  return price
 }
+
 
 
 // Front End(User)
@@ -19,9 +37,10 @@ $(document).ready(function() {
     var userSize = parseInt($("input:radio[name=size]:checked").val());
     console.log(userSize)
     $("input:checkbox[name=topping]:checked").each(function(){
-      userToppings.push($(this).val());
+      userToppings.push($(this).val())
       console.log(userToppings)
     })
-    var pizzaPrice = new Pizza(userSize, userToppings);
+    var newPizza = new Pizza(userSize, userToppings);
+    var pizzaPrice = newPizza.priceCalculate()
     });
   });
