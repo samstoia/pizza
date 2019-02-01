c99f008b7d89cd98bcb47037ae5d1fcecdb45899
samstoia <samstoia@gmail.com>
2019-02-01 11:12:59 -0800

    create function for pushing toppings into array

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 // Back End(Business)
 
-var Pizza(size, toppings) {
+function Pizza(size, toppings) {
   this.size = size,
   this.toppings = toppings
 }
@@ -11,13 +11,17 @@ Pizza.prototype.priceShow = function(pizzaPrice) {
 
 
 // Front End(User)
+var userToppings = [];
 
 $(document).ready(function() {
   $("form#pizzaMenu").submit(function(event) {
     event.preventDefault();
     var userSize = parseInt($("input:radio[name=size]:checked").val());
-    var userToppings = parseInt($("input:radio[name=toppings]:checked").val());
-    var pizzaPrice = new Pizza(size, toppings);
-
-    
+    console.log(userSize)
+    $("input:checkbox[name=topping]:checked").each(function(){
+      userToppings.push($(this).val());
+      console.log(userToppings)
+    })
+    var pizzaPrice = new Pizza(userSize, userToppings);
+    });
   });
